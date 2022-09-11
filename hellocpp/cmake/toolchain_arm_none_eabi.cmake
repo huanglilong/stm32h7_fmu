c16eb3aa641c77b87dbc2ad67fd850fde46f80aa
huanglilong <huanglilongwk@outlook.com>
2022-09-11 18:58:47 +0800

    add C++ and C flags

--- hellocpp/cmake/toolchain_arm_none_eabi.cmake
+++ hellocpp/cmake/toolchain_arm_none_eabi.cmake
@@ -37,6 +37,80 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CPU_FLAGS     "-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard" CACHE STRING "" FORCE)
 set(AC_HW_FLAGS   "${CPU_FLAGS} -pipe -isystem ${NUTTX_PATH}/include" CACHE STRING "" FORCE)
 
+# C++ and C flags
+# ref: cmake/px4_add_common_flags.cmake
+add_compile_options(
+    -g # always build debug symbols
+    # optimization options
+    -fdata-sections
+    -ffunction-sections
+    -fomit-frame-pointer
+    -fmerge-all-constants
+    #-funsafe-math-optimizations # Enables -fno-signed-zeros, -fno-trapping-math, -fassociative-math and -freciprocal-math
+    -fno-signed-zeros	# Allow optimizations for floating-point arithmetic that ignore the signedness of zero
+    -fno-trapping-math	# Compile code assuming that floating-point operations cannot generate user-visible traps
+    #-fassociative-math	# Allow re-association of operands in series of floating-point operations
+    -freciprocal-math	# Allow the reciprocal of a value to be used instead of dividing by the value if this enables optimizations
+    -fno-math-errno		# Do not set errno after calling math functions that are executed with a single instruction, e.g., sqrt
+    -fno-strict-aliasing
+    # Warnings
+    -Wall
+    -Wextra
+    -Werror
+    -Warray-bounds
+    -Wcast-align
+    -Wdisabled-optimization
+    -Wdouble-promotion
+    -Wfatal-errors
+    -Wfloat-equal
+    -Wformat-security
+    -Winit-self
+    -Wlogical-op
+    -Wpointer-arith
+    -Wshadow
+    -Wuninitialized
+    -Wunknown-pragmas
+    -Wunused-variable
+    # disabled warnings
+    -Wno-missing-field-initializers
+    -Wno-missing-include-dirs # TODO: fix and enable
+    -Wno-unused-parameter
+)
+# force color for gcc > 4.9
+add_compile_options(-fdiagnostics-color=always)
+add_compile_options(
+    -fno-builtin-printf
+    -fno-strength-reduce
+    -Wformat=1
+    -Wunused-but-set-variable
+    -Wno-format-truncation # TODO: fix
+)
+
+# C only flags
+set(C_FLAGS)
+list(APPEND c_flags
+    -fno-common
+    -Wbad-function-cast
+    -Wnested-externs
+    -Wstrict-prototypes
+)
+foreach(flag ${C_FLAGS})
+    add_compile_options($<$<COMPILE_LANGUAGE:C>:${flag}>)
+endforeach()
+
+# CXX only flags
+set(CXX_FLAGS)
+list(APPEND CXX_FLAGS
+    -fno-exceptions
+    -fno-threadsafe-statics
+    -Wreorder
+    # disabled warnings
+    -Wno-overloaded-virtual # TODO: fix and remove
+)
+foreach(flag ${CXX_FLAGS})
+    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:${flag}>)
+endforeach()
+
 # find gcc library
 execute_process(
     COMMAND ${CMAKE_C_COMPILER} -mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -print-file-name=libgcc.a
